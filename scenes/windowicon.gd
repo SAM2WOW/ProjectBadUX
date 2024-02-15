dc42dae42d1baf3cb7161454f987c08697834b48
Sam Feng <fengyuan20000303@gmail.com>
2024-02-15 02:46:38 -0800

    Confirm Shaming

--- scenes/windowicon.gd
+++ scenes/windowicon.gd
@@ -5,8 +5,13 @@ var window
 
 
 func _ready():
-	set_button_icon(window.window_icon)
-	set_tooltip_text(window.window_name)
+	if "window_icon" in window:
+		set_button_icon(window.window_icon)
+	
+	if "window_name" in window:
+		set_tooltip_text(window.window_name)
+	else:
+		set_tooltip_text(window.name)
 
 
 func _on_pressed():
---
+++ scenes/windows/trick_confirm.gd
@@ -0,0 +1,65 @@
+extends Window
+
+signal confirmed
+signal canceled
+
+@export var window_icon : Texture2D
+@export var window_name : String
+
+@export var second_trick = false
+
+var trick_finished = false
+var tween
+
+
+func _ready():
+	set_title(window_name)
+	
+	await get_tree().create_timer(1).timeout
+	
+	$Node2D/No.show()
+
+
+func _on_close_requested():
+	pass
+
+
+func _on_no_pressed():
+	Global.windowsManager.add_window($ConfirmationDialog)
+	$ConfirmationDialog.show()
+	$ConfirmationDialog.popup_centered()
+
+
+func _on_yes_pressed():
+	emit_signal("confirmed")
+	
+	Global.windowsManager.remove_window(self)
+	queue_free()
+
+
+func _on_no_mouse_entered():
+	if not trick_finished:
+		trick_finished = true
+		
+		$Node2D/No.set_flat(false)
+		$Node2D/Confirm.set_flat(true)
+		
+		var yes_pos = $Node2D/Confirm.get_position()
+		var no_pos = $Node2D/No.get_position()
+		
+		tween = create_tween()
+		tween.tween_property($Node2D/Confirm, "position", no_pos, 0.1)
+		tween.parallel().tween_property($Node2D/No, "position", yes_pos, 0.1)
+
+
+func _on_confirmation_dialog_canceled():
+	Global.windowsManager.remove_window($ConfirmationDialog)
+	$ConfirmationDialog.hide()
+
+
+func _on_confirmation_dialog_confirmed():
+	emit_signal("canceled")
+	
+	Global.windowsManager.remove_window($ConfirmationDialog)
+	Global.windowsManager.remove_window(self)
+	queue_free()
