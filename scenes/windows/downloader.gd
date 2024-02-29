d3b58e203e54e21067842cd9ffe50363021f46c1
Sam Feng <fengyuan20000303@gmail.com>
2024-02-29 01:57:46 -0800

    Download Btn Spawning

--- scenes/windows/downloader.gd
+++ scenes/windows/downloader.gd
@@ -1,1 +1,13 @@
 extends "res://scenes/window.gd"
+
+
+var download = preload("res://scenes/download_buttons.tscn")
+
+func _ready():
+	super._ready()
+	
+	for i in range(20):
+		var d = download.instantiate()
+		d.set_scale(Vector2(0.5, 0.5))
+		$Node2D.add_child(d)
+		d.set_position(Vector2(randf_range(12, 500), randf_range(12, 500)))
