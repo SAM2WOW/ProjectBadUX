af57f19a0bbc77c9b5ddcd06cd919bcef950da86
Sam Feng <fengyuan20000303@gmail.com>
2024-03-08 22:45:38 -0800

    femail update

--- scenes/windows/femail.gd
+++ scenes/windows/femail.gd
@@ -3,3 +3,11 @@ extends "res://scenes/window.gd"
 
 func _ready():
 	super._ready()
+
+
+func _on_archive_pressed():
+	$Control/MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/TabContainer.set_current_tab(1)
+
+
+func _on_inbox_pressed():
+	$Control/MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/TabContainer.set_current_tab(0)
