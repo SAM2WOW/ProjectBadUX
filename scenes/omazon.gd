af985b09dce46f9a94c225cc18f2d6c00828d343
Sam Feng <fengyuan20000303@gmail.com>
2024-03-19 01:39:38 -0700

    Update omazon.gd

--- scenes/omazon.gd
+++ scenes/omazon.gd
@@ -59,5 +59,5 @@ func _on_shampoo_ads_pressed():
 
 func _on_shampoo_tip_pressed():
 	SoundPlayer.play("Confirm")
-	var w = dogshampoo.instantiate()
+	var w = tips.instantiate()
 	Global.windowsManager.add_window(w)
